2246e60540c981abb3e8b9cf9ea2d7b73623fd14
yarik <yarik.si89spb@gmail.com>
2024-06-18 18:41:30 +0800

    Slightly re-style chat winddow

--- client/src/components/ChatComments.tsx
+++ client/src/components/ChatComments.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Comment } from '../utils/interfaces';
+import './ChatComments.css'
 
 interface ChatCommentsProps {
   comments: Comment[];
@@ -9,11 +10,11 @@ const ChatComments: React.FC<ChatCommentsProps> = ({ comments }) => {
   return (
     <ul className='list-group list-group'>
       {comments.map((comment, index) => (
-        <li key={index} className='list-group-item d-flex justify-content-between align-items-start'>
-          <div className="text-wrap" style={{width: '30rem'}}>
+        <li key={index} className='list-group-item d-flex justify-content-between align-items-start comment-line'>
+          <div className="text-wrap user-name" style={{width: '20rem'}}>
             {comment.userName}
           </div>
-          <div className="text-wrap" style={{width: '30rem'}}>
+          <div className="text-wrap user-comment" style={{width: '70rem'}}>
             {comment.text}
           </div>
         </li>
