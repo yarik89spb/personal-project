6081111651ae531586d367c466fac475c05a4f2c
yarik <yarik.si89spb@gmail.com>
2024-06-20 20:19:05 +0800

    Add python file to fecth comments from db and clean / tokenzie / get counts of comments

---
+++ server/nlp/user_comments.py
@@ -0,0 +1,66 @@
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+from pymongo import MongoClient
+import pandas as pd
+import numpy as np
+import jieba
+import string
+import stopwordsiso as sw
+from collections import Counter
+
+
+env_path = Path('..') / '.env'
+load_dotenv(dotenv_path=env_path)
+database_name = os.getenv('MONGO_DATABASE_NAME')
+database_connection_string = os.getenv('MONGO_CONNECTION_STRING')
+
+class AtlasClient ():
+  def __init__ (self, connection_string, database_name):
+    self.mongodb_client = MongoClient(connection_string)
+    self.database = self.mongodb_client[database_name]
+
+  ## A quick way to test if we can connect to Atlas instance
+  def ping (self):
+    self.mongodb_client.admin.command('ping')
+
+  def get_collection (self, collection_name):
+    collection = self.database[collection_name]
+    return collection
+
+  def find (self, collection_name, filter = {}, limit=0):
+    collection = self.database[collection_name]
+    items = list(collection.find(filter=filter, limit=limit))
+    return items
+  
+  def insert_word_count (self, collection_name, word, count):
+    collection = self.database[collection_name]
+    collection.insert_one({"word": word, "count": count})
+  
+
+atlas_client = AtlasClient (database_connection_string, database_name)
+project_data = atlas_client.find('project-responses', filter={'projectId':"c75a22fs68cgs3"}, limit=1)
+# Since mongo.find always going to return array, get the first (=only) element 
+project_data = project_data[0]
+# Store comments in array of arrays 
+comments = [q['comments'] for q in project_data['questions'] if len(q['comments']) > 0]
+# Flatten to 1D array
+comments_flat =  np.concatenate(comments)
+# Create Pandase dataframe
+comments_df = pd.DataFrame.from_records(comments_flat).set_index('_id', drop=True)
+# Remove punctuation using lookup table 
+extra_punct = '^#@$%￥…《》“”{}*&～；【】（）-+_=!?；；（），。、：《》‘’—' 
+trans_table = str.maketrans('', '', string.punctuation + extra_punct)
+# Apply translation to remove punctuation and store in new column
+comments_df['text_clean'] = comments_df['text'].apply(lambda x: x.strip().translate(trans_table))
+# Tokenize Chinese words
+comments_df['text_split'] = comments_df['text_clean'].apply(lambda x: list(jieba.cut(x)))
+custom_stopwords = (sw.stopwords("zh")).union(sw.stopwords("en")).union({
+  ' ', '', '妳', '你', '他', '她', '它'})
+comments_df['text_sanitzized'] = comments_df['text_split'].apply(lambda x: [w for w in x if w not in custom_stopwords])
+
+comments_all = list(np.concatenate(comments_df['text_sanitzized']))
+word_counts = Counter(comments_all)
+# Store top 50 words
+for word, count in word_counts.items():
+    collection.insert_one({"word": word, "count": count})
