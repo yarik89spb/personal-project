c8b30888b273556f063072dd7421d57b0ddc498c
yarik <yarik.si89spb@gmail.com>
2024-07-17 14:18:04 +0800

    Restlye some buttons and animations

--- client/src/components/ReactionButtons.tsx
+++ client/src/components/ReactionButtons.tsx
@@ -10,13 +10,13 @@ const ReactionButtons: React.FC<ReactionButtonsProps> = ({ handleEmojiClick, sel
   return (
     <div className='reaction-buttons'>
       <button className={`reaction-button ${selectedEmoji=== 'heart' ? 'selected' : 'deselected'}`} onClick={() => handleEmojiClick('heart', true)}>
-        <FontAwesomeIcon icon={faHeart} />
+        <FontAwesomeIcon icon={faHeart} className="fa-icon heart"/>
       </button>
       <button className={`reaction-button ${selectedEmoji === 'like' ? 'selected' : 'deselected'}`} onClick={() => handleEmojiClick('like', true)}>
-        <FontAwesomeIcon icon={faThumbsUp} />
+        <FontAwesomeIcon icon={faThumbsUp} className="fa-icon like"/>
       </button>
       <button className={`reaction-button ${selectedEmoji === 'dislike' ? 'selected' : 'deselected'}`} onClick={() => handleEmojiClick('dislike', false)}>
-        <FontAwesomeIcon icon={faThumbsDown} />
+        <FontAwesomeIcon icon={faThumbsDown} className="fa-icon dislike"/>
       </button>
       {/* Add more reaction buttons as needed */}
     </div>
